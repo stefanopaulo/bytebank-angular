f34bbeed5f8b42e346ea5181929caae61c63d278
Stefano Paulo <stefanopaulo47@gmail.com>
2021-05-28 16:51:00 -0300

    criando primeiro component

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'nova-tranferencia',
+    templateUrl: './nova-tranferencia.component.html',
+    styles: ['./nova-transferencia.component.css']
+})
+
+export class NovaTransferenciaComponent {
+
+}
