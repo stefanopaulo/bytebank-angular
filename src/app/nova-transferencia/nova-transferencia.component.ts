152148910e0dbfc246643d69d02fb90880dcf33a
Stefano Paulo <stefanopaulo47@gmail.com>
2021-05-28 19:24:21 -0300

    estilizando o formulario

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-nova-tranferencia',
     templateUrl: './nova-transferencia.component.html',
-    styles: ['./nova-transferencia.component.scss']
+    styleUrls: ['./nova-transferencia.component.scss']
 })
 
 export class NovaTransferenciaComponent {
